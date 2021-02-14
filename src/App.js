9c6fa0625144a560ba12cdbf94260c6a215e08b3
Jirka Kral <jurashek.king@gmail.com>
2021-02-14 22:21:29 +0100

    Added basic functionality to form component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from "react";
 import "./App.css";
 import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            allTodos: [],
-        };
-    }
-    render() {
-        return (
-            <Form />
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      allTodos: [],
+    };
+    this.addTodoHandler = this.addTodoHandler.bind(this);
+  }
+
+  addTodoHandler(todoText) {
+    this.setState((state) => {
+      return {
+        allTodos: [...this.state.allTodos, { todo: todoText, completed: false, id: Date.now() },
+        ],
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form onSubmit={this.addTodoHandler} />
+        <TodoList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoName: "",
+        };
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.onSubmitHandler = this.onSubmitHandler.bind(this);
+    }
+
+    onChangeHandler(event) {
+        this.setState({todoName: event.target.value});
+    }
+
+    onSubmitHandler(event) {
+        event.preventDefault();
+        if(this.state.todoName !== "") {
+            this.props.onSubmit(this.state.todoName);
+            this.setState({todoName: ""});
+        }
+        return;
+    }
+    
+
   render() {
     return (
-      <form>
-        <input type="text" className="todo-input"></input>
+      <form onSubmit={this.onSubmitHandler}>
+        <input value={this.state.todoName} onChange={this.onChangeHandler} type="text" className="todo-input" />
         <button type="submit" className="todo-button">
           <i className="fas fa-plus-square"></i>
         </button>
